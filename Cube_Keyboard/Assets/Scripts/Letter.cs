b5f532b7efd419188c9ce0ad4a296b31365479c5
Marcelo Nunes <marcelojcnunes@hotmail.com>
2019-10-26 16:05:22 +0100

    - added a outline to each key

---
+++ Cube_Keyboard/Assets/Scripts/Letter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Letter : MonoBehaviour
+{
+    public string value;
+
+    public Shader outlineShader;
+    private void OnMouseEnter() 
+    {
+        //Fetch the Material from the Renderer of the GameObject
+        Material material = GetComponent<Renderer>().material;
+        material.shader = Shader.Find("Outlined/Uniform");
+    }
+
+    private void OnMouseExit()
+    {
+        //Fetch the Material from the Renderer of the GameObject
+        Material material = GetComponent<Renderer>().material;
+        material.shader = Shader.Find("Standard");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(value);
+    }
+
+
+}
