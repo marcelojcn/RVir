bc5f33213ffbcade6f74f37e366ae3ec6a39ad94
Marcelo Nunes <marcelojcnunes@hotmail.com>
2019-10-26 17:43:51 +0100

    - draws diacritics

--- Cube_Keyboard/Assets/Scripts/Letter.cs
+++ Cube_Keyboard/Assets/Scripts/Letter.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +11,18 @@ using UnityEngine.UI;
 public class Letter : MonoBehaviour
 {
     public string value;
+    public bool IsDiacritic;
 
-    public Shader outlineShader;
+    public List<GameObject> Diacritics;
+    private List<GameObject> CreatedDiacritics;
+
+    private DateTime? MouseOverTimestamp;
+
+    private void Start()
+    {
+        CreatedDiacritics = new List<GameObject>();
+    }
+
     private void OnMouseEnter() 
     {
         //Fetch the Material from the Renderer of the GameObject
@@ -21,12 +35,100 @@ public class Letter : MonoBehaviour
         //Fetch the Material from the Renderer of the GameObject
         Material material = GetComponent<Renderer>().material;
         material.shader = Shader.Find("Standard");
+
+        // If not a Diacritic and has Diacritics and Timestamp saved
+        if (!IsDiacritic && Diacritics.Any() && MouseOverTimestamp.HasValue)
+        {
+            MouseOverTimestamp = null;
+        }
     }
 
+    private void OnMouseOver()
+    {
+        // If not a Diacritic and has Diacritics and no Timestamp saved
+        if (!IsDiacritic && Diacritics.Any() && !MouseOverTimestamp.HasValue)
+        {
+            // Save the timestamp of the moment the user started hovering the key
+            MouseOverTimestamp = DateTime.Now;
+        }
+
+        // If it is not a diacritic and none of the diacritic were created 
+        if (!IsDiacritic && !CreatedDiacritics.Any() && Diacritics.Any() && (DateTime.Now - MouseOverTimestamp.Value).Seconds >= 2)
+        {
+            int index = 0;
+
+            // Draw Diacritics
+            for (float y = (transform.position.y + 1.08f); y >= (transform.position.y - 1.08f); y -= 1.08f)
+            {
+                for (float x = (transform.position.x - 1.08f); x <= (transform.position.x + 1.08f); x += 1.08f)
+                {
+                    if (!(x == transform.position.x && y == transform.position.y) && index < Diacritics.Count) 
+                    {
+                        // Instantiate the Diacritic
+                        GameObject newDiacritic = Instantiate(Diacritics[index], new Vector3(x, y, transform.position.z), Quaternion.identity);
+
+                        // Save the created Diacritic's object
+                        CreatedDiacritics.Add(newDiacritic);
+
+                        index++;
+                    }
+                }
+            }
+        }
+    }
     private void OnMouseDown()
     {
+        // Write Letter Value
         Debug.Log(value);
+
+        if (IsDiacritic)
+        {
+            // Get Main Letter
+            Letter letter = GameObject.Find($"{RemoveDiacritics(value)}(Clone)").gameObject.GetComponent<Letter>();
+
+            // Clean Letter
+            letter.Clean();
+        }
+        else 
+        {
+            // Clean himself
+            Clean();
+        }
     }
 
 
+    public void Clean() 
+    {
+        // If this Letter has Created Diacritics
+        if (CreatedDiacritics.Any()) 
+        {
+            // Delete all
+            CreatedDiacritics.ForEach(d => Destroy(d));
+
+            // Reset CreatedDiacritics list
+            CreatedDiacritics = new List<GameObject>();
+        }
+
+        // If a letter was written then clean MouseOverTimeStamp 
+        MouseOverTimestamp = null;
+    }
+
+    #region Protected Methods
+    protected string RemoveDiacritics(string text)
+    {
+        var normalizedString = text.Normalize(NormalizationForm.FormD);
+        var stringBuilder = new StringBuilder();
+
+        foreach (var c in normalizedString)
+        {
+            var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+            {
+                stringBuilder.Append(c);
+            }
+        }
+
+        return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+    }
+    #endregion
 }
