a7acdea2ffe88c30b8244406f83581d133325b96
Marcelo Nunes <marcelojcnunes@hotmail.com>
2019-10-30 01:39:47 +0000

    -  cube rotation

--- Cube_Keyboard/Assets/CubeController.cs
+++ Cube_Keyboard/Assets/CubeController.cs
@@ -20,6 +20,8 @@ public class CubeController : MonoBehaviour
     }
     public void generateColoredCubeSquare()
     {
+        GameObject mainCube = GameObject.Find("MainCube");
+
         Camera camera = Camera.main;
         GameObject sim = GameObject.Find("[VRSimulator_CameraRig]");
         Vector3 pos = camera.transform.position;
@@ -27,7 +29,10 @@ public class CubeController : MonoBehaviour
         Quaternion rot = camera.transform.rotation;
         float spawnDis = 10;
         Vector3 destination = pos + dir * spawnDis;
-       
+
+
+        mainCube.transform.position = new Vector3(1.08f, 1.92f, 1.08f) + destination;
+        mainCube.transform.eulerAngles = new Vector3(0.0f, rot.eulerAngles.y + +135.0f, 0.0f);
 
         for (float z = 0; z < 3; z += 1.08f)
         {
@@ -37,11 +42,11 @@ public class CubeController : MonoBehaviour
                 {
                     // Check if not the key in the cube's middle
                     if (!(x == 1.08f && y == 1.92f && z == 1.08f))
-                    {   
+                    {
                         // Instantiate the key
-                        Instantiate(CubePrefabList[index], new Vector3(x, y, z) + destination, Quaternion.identity);
+                        var key = Instantiate(CubePrefabList[index], new Vector3(x, y, z) + destination, Quaternion.identity);
+                        key.transform.parent = mainCube.transform;
 
-                        
                         // Increase index
                         if (index < CubePrefabList.Count)
                         {
@@ -51,6 +56,10 @@ public class CubeController : MonoBehaviour
                 }
             }
         }
+
+        
+
+        
 
 
     }
