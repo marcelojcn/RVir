79e0428a2aeac793669d022080da019fb35db4c6
loure <mrlore473@gmail.com>
2019-11-10 16:53:39 +0000

    Timer

--- Cube_Keyboard/Assets/Scripts/TimerController.cs
+++ Cube_Keyboard/Assets/Scripts/TimerController.cs
@@ -4,22 +4,21 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
-
-public class GameManager : MonoBehaviour
+public class TimerController : MonoBehaviour
 {
     private float _timeLeft = 10.0f;
     public Text _startText;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _timeLeft-= Time.deltaTime;
-        
+        _timeLeft -= Time.deltaTime;
+
         _startText.text = "Time Left " + (_timeLeft).ToString("0");
         if (_timeLeft <= 0)
             GameOver();
